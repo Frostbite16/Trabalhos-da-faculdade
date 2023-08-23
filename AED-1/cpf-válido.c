38f97ed77143cac511ea0cd1c3a656e17ff82507
Frostbite16 <84871179+Frostbite16@users.noreply.github.com>
2023-08-23 11:10:10 -0400

    Update cpf-válido.c

--- AED-1/cpf-válido.c
+++ AED-1/cpf-válido.c
@@ -8,12 +8,18 @@ int obtem_primeiro_digito_verificador(char cpf[]){//obtem o primeiro digito veri
         digito+=intcpf*cont;
         cont-=1;
     }
-    return(digito*10%11);
+    if(digito*10%11==10){
+        digito=0;
+    }
+    else{
+        digito=digito*10%11;
+    }
+    return(digito);
 }
 int obtem_segundo_digito_verificador(char cpf[]) {//obtem o primeiro digito verificador
     int digito=0,intcpf,cont=11,calccpf,i;//declaracao de variaveis
     for(int i=0;i<10;i++){//converte digitos do cpf de char para int e faz o calculo necessario
-        if(i<=9){
+        if(i<=8){
             intcpf = cpf[i] - '0';
         }
         else{
@@ -22,22 +28,32 @@ int obtem_segundo_digito_verificador(char cpf[]) {//obtem o primeiro digito veri
         digito+=intcpf*cont;
         cont-=1;
     }
+    if(digito*10%11==10){
+        digito=0;
+    }
+    else{
+        digito=digito*10%11;
+    }
     return(digito);
 }
 int verifica_cpf_valido(char cpf[]) {
-    int d1cpf=cpf[9]-'0',d2cpf=d2cpf=cpf[10]-'0',valido=1,count=0;
-    float d1,d2;//declaracao de variaveis
-    d1=obtem_primeiro_digito_verificador(cpf)*10%11;
-    d2=obtem_segundo_digito_verificador(cpf)*10%11;
-    if(d1==10)
-        d1=0;
-    if(d2==10)
-        d2=0;
-    for(int i=0;i<11;i++){//exceções da regra, EX: 111.111.111-11
+    int d1cpf=cpf[12]-'0',d2cpf=cpf[13]-'0',valido=1,count=0,d1,d2,c=0;//declaracao de variaveis
+    char cpf2[12];
+    for(int i=0;i<15;i++){
+        if((cpf[i]=='.')||(cpf[i]=='-')){
+            ++i;
+        }
         if(d1cpf==(cpf[i]-'0')){
             count++;
         }
+        if(c<=11){
+            cpf2[c]=cpf[i];
+            
+        }
+        c++;
     }
+    d1=obtem_primeiro_digito_verificador(cpf2);
+    d2=obtem_segundo_digito_verificador(cpf2);
     if((d1cpf==d1)&&(d2cpf==d2)&&(count<11)){//verificação do cpf
         valido=1;
     }
@@ -48,7 +64,9 @@ int verifica_cpf_valido(char cpf[]) {
 }
 int main(){
     char cpf[12];
-    scanf("%s,",&cpf);
-    printf("%d, %d",obtem_primeiro_digito_verificador(cpf),obtem_segundo_digito_verificador(cpf));
+    char icpf[15];
+    scanf("%s",&cpf);
+    scanf("%s",&icpf);
+    printf("%d, %d, %d",obtem_primeiro_digito_verificador(cpf),obtem_segundo_digito_verificador(cpf), verifica_cpf_valido(icpf));
     
 }
